d5d2fd41701bddcc908de299f42e4e7607459251
Jerry Boyaji <jerryboyaji@gmail.com>
2020-05-17 18:24:14 +1000

    main menu

--- BubblePop/ViewController.swift
+++ BubblePop/ViewController.swift
@@ -8,13 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class HomeController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        return identifier == "PlayGameSegue"
+            ? validateNameTextField()
+            : true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let target = segue.destination as? GameViewController {
+            //save the name to the list in userdefaults
+            let defaults = UserDefaults.standard
+            var playerList = defaults.object(forKey: "PlayerList") as? [String] ?? [String]()
+            if let playerName = nameTextField.text, playerName != "" {
+                if !playerList.contains(playerName) {
+                    playerList.append(playerName)
+                }
+            }
+            else {
+                nameTextField.layer.borderColor = UIColor(named: "red")?.cgColor
+                return
+            }
+        }
+    }
+    
+    @IBAction func nameTextFieldChanged(_ sender: Any) {
+        validateNameTextField()
+    }
+    
+    func validateNameTextField() -> Bool {
+        if let name = nameTextField.text, name != "" {
+            nameTextField.layer.borderWidth = 0.0
+            
+            return true
+        }
+        else {
+            nameTextField.layer.borderColor = UIColor.red.cgColor
+            nameTextField.layer.borderWidth = 1.0
+            nameTextField.layer.cornerRadius = 4.0
+            nameTextField.layer.masksToBounds = true
+            
+            return false
+        }
+    }
 }
-
